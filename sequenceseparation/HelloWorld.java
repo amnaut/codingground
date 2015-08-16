4308bebf8708f5883cfccb8586e01a248189d95f
amnaut <amnaut@gmail.com>
2015-08-15 21:50:11 -0400

    Created by Coding Ground Client

---
+++ sequenceseparation/HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld{
+    public static void separateNumber(String s){
+    int len=s.length();
+    StringBuilder sb= new StringBuilder();
+    //if (len==1)
+      //  sb.append(s.charAt(0));
+    for(int i =0;i<len-1;i++){
+        sb.append(s.charAt(i));
+        if (s.charAt(i+1)-s.charAt(i)!=1)
+            sb.append(';');
+            
+    }
+    sb.append(s.charAt(len-1));
+    System.out.println(sb.toString());
+
+
+}
+     public static void main(String []args){
+        System.out.println("Hello World");
+        String s = "135";
+        separateNumber(s);
+     }
+}
